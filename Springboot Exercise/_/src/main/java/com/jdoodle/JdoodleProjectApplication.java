fa0fb2fcc58aa7775e54a7c92435ad1cbadeafe3
Payal Sahu <141853271+payalsahu1303@users.noreply.github.com>
2025-11-05 17:06:32 +0530

    Add files via upload

---
+++ Springboot Exercise/_/src/main/java/com/jdoodle/JdoodleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jdoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdoodleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdoodleProjectApplication.class, args);
+}
+
+}
